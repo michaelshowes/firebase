3ac400455de39b85b1ebdbc5fc48ef051307f0f9
Michael Showes <michael.showes@gmail.com>
2023-04-29 19:20:00 -0400

    added image preview to uploads

--- src/components/SendImages.tsx
+++ src/components/SendImages.tsx
@@ -1,14 +1,29 @@
 import { useState } from 'react';
 import { storage } from '../config/firebase';
 import { ref, uploadBytes } from 'firebase/storage';
+import { v4 } from 'uuid';
 
 export default function SendImages() {
 	const [fileUpload, setFileUpload] = useState<File>();
+	const [imagePreviewUrl, setImagePreviewUrl] = useState('');
+
+	function handleFileSelect(file: File) {
+		setFileUpload(file);
+
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			setImagePreviewUrl(reader.result as string);
+		};
+		reader.readAsDataURL(file as Blob);
+	}
 
 	async function uploadFile() {
 		if (!fileUpload) return;
 
-		const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
+		const filesFolderRef = ref(
+			storage,
+			`projectFiles/new/${fileUpload.name + v4()}`
+		);
 
 		try {
 			await uploadBytes(filesFolderRef, fileUpload);
@@ -21,9 +36,18 @@ export default function SendImages() {
 		<div>
 			<input
 				type='file'
-				onChange={(e) => setFileUpload(e.target.files?.[0])}
+				onChange={(e) => handleFileSelect(e.target.files?.[0] as File)}
 			/>
 			<button onClick={uploadFile}>Upload File</button>
+			<br />
+			<br />
+			{imagePreviewUrl && (
+				<img
+					src={imagePreviewUrl}
+					alt='preview'
+					width='300'
+				/>
+			)}
 		</div>
 	);
 }
